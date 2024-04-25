0f5bdb6c693e6fc975e9b34580ad72145da1d16e
Andreas Liodden <andreasliodd1@gmail.com>
2024-04-25 16:19:26 +0200

    Reduced tick between projectiles

--- src/main/java/no/uib/inf101/sample/controller/GameController.java
+++ src/main/java/no/uib/inf101/sample/controller/GameController.java
@@ -19,7 +19,7 @@ public class GameController implements KeyListener {
     private static final int UPDATE_ENEMY_TICK = 30;
     private static final int ADD_SCORE_TICK = 100;
     private static final int GAPPLE_COUNTDOWN_TICK = 100;
-    private static final int ADD_PROJECTILE_TICK = 480;
+    private static final int ADD_PROJECTILE_TICK = 450;
     private static final int ENEMY_READY_TICK = ADD_PROJECTILE_TICK - 2 * UPDATE_ENEMY_TICK;
     private static final double RESET_GAME_TICK = 750;
     private static final int TICK_DIVISOR = 3;
